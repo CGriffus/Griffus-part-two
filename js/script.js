09742207d11898c388f941abe5728c5740f1a4ce
Caitlin <caitlin@griffus.com>
2019-06-19 15:37:23 -0400

    Added comments and modified temp ranges

--- js/script.js
+++ js/script.js
@@ -3,25 +3,27 @@ var tempFahr = prompt('What is the temperature?');
 var result = 'Since it is '+tempFahr+' degrees and you are going to a '+eventType+' event, you should wear';
 
 
-
-if (eventType == 'casual' && tempFahr < 54) {
+//Casual
+if (eventType === 'casual' && tempFahr < 54) {
     console.log(result +' something comfy and a coat.');
+} else if (eventType === 'casual' && (tempFahr >= 54 && tempFahr <=70)) {
+    console.log(result + ' something comfy and a jacket.');
 } else if (eventType === 'casual' && tempFahr > 70) {
     console.log(result +' something comfy and no jacket.');
-} else if (eventType === 'casual' && tempFahr >= 54 || tempFahr <=70) {
-    console.log(result + ' something comfy and a jacket.')
+//Semi-formal
 } else if (eventType === 'semi-formal' && tempFahr < 54) {
-    console.log(result + ' a polo and a coat.')
-} else if (eventType === 'semi-formal' && tempFahr >=54 || tempFahr <= 70) {
-    console.log(result + ' a polo and a jacket.')
+    console.log(result + ' a polo and a coat.');
+} else if (eventType === 'semi-formal' && (tempFahr >= 54 && tempFahr <=70)) {
+    console.log(result + ' a polo and a jacket.');
 } else if (eventType === 'semi-formal' && tempFahr > 70) {
-    console.log(result + ' a polo and no jacket.')
+    console.log(result + ' a polo and no jacket.');
+//Formal
 } else if (eventType === 'formal' && tempFahr < 54) {
-    console.log(result + ' a suit and a coat.')
-} else if (eventType === 'formal' && tempFahr >= 54 || tempFahr <= 70) {
-    console.log(result + ' a suit and a jacket.')
-} else if(eventType === 'formal' && tempFahr > 70) {
-    console.log(result + ' a suit and no jacket.')
+    console.log(result + ' a suit and a coat.');
+} else if (eventType === 'formal' && (tempFahr >= 54 && tempFahr <=70)) {
+    console.log(result + ' a suit and a jacket.');
+} else if (eventType === 'formal' && tempFahr > 70) {
+    console.log(result + ' a suit and no jacket.');
 }
     
 
